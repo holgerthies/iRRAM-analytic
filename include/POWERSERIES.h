85a1fb72ba6d91d67315ef9fb32dc32271645de8
Holger Thies <info@holgerthies.com>
2015-11-27 19:40:26 +0900

    Updated powerseries to work with variadic templates instead of vectorized functions.

--- include/POWERSERIES.h
+++ include/POWERSERIES.h
@@ -10,131 +10,99 @@
 #include "combinatorics.h"
 
 namespace iRRAM{
-namespace PWRSERIES_IMPL{ // implementation details for having special type for dimension 0
-template <unsigned int n, class T>
-  class POWERSERIES;
-// a powerseries of dimension 0 should just be a scalar
-// so POWERSERIES<0,T> should be an alias for T
-template <unsigned int n, class T>
-struct PWRSERIES_TYPE{
-typedef POWERSERIES<n,T> type;   
-};
+  namespace PWRSERIES_IMPL{ // implementation details for having special type for dimension 0
+    template <unsigned int n, class T>
+    class POWERSERIES;
+    // define coeff type 
+    template <unsigned int n, class T>
+    struct COEFF_TYPE{
+      typedef POWERSERIES<n-1,T> type;   
+    };
 
-template <class T>
-struct PWRSERIES_TYPE<0,T>{
-typedef T type;
-};
+    template <class T>
+    struct COEFF_TYPE<1,T>{
+      typedef T type;   
+    };
 
-template <unsigned int n, class T>
-class POWERSERIES{
-using seq = std::function<T(const std::vector<unsigned long>&)>;
-using sq_ptr = std::shared_ptr<seq>;
-private:
-sq_ptr f;
-public:
-    POWERSERIES(sq_ptr f): f(f) {};
-    // get the coefficient with given index
-    POWERSERIES(seq  f): POWERSERIES(sq_ptr(new seq(f))) {};
-    T get(const std::vector<unsigned long>& v) const {
-    return (*f)(v);
-    }
-    typename PWRSERIES_TYPE<n-1,T>::type  operator[](const unsigned long) const;
+    template <unsigned int n, class T>
+    class POWERSERIES{
+      using coeff_type = typename COEFF_TYPE<n,T>::type;
+      using seq = std::function<coeff_type(const unsigned long)>;
+      using sq_ptr = std::shared_ptr<seq>;
+    private:
+      sq_ptr f;
+    public:
+      POWERSERIES(T x): POWERSERIES([x] (unsigned int i) {
+	  if(i==0){
+	    return coeff_type(x);
+	  } 
+	  else{
+	    return coeff_type(T());
+	  }
+	}) {}; 
+      POWERSERIES() : POWERSERIES(T()) {};
+      POWERSERIES(sq_ptr f): f(f) {};
+      POWERSERIES(seq f) : f(sq_ptr(new seq(f))) {}; 
+      // constructor from function N^n -> T
+      template <typename... ARGS>
+      POWERSERIES(std::function<T(const unsigned long, ARGS...)>);
+      // get the coefficient with given index
+      //POWERSERIES(seq  f): POWERSERIES(sq_ptr(new seq(f))) {};
+      //T get(const std::vector<unsigned long>& v) const {
+      //  return (*f)(v);
+      //}
+      coeff_type  operator[](const unsigned long) const;
 
-    int known_coeffs() const{
-    return 0;
-    }
-};
-
-
-
-
-template <class T>
-POWERSERIES<1,T> derive(POWERSERIES<1,T>& pwr, const int d){
-  return POWERSERIES<1,T>(std::shared_ptr<std::function<T(unsigned long)>>(new std::function<T(unsigned long)>([&pwr,d] (unsigned long i) {
-			T prod(1);
-				for (unsigned int j = 0; j < d; j++)
-					prod *= i+d-j;
-				return prod * pwr.get({ i+d });
-        })));
-}
-
-// partially evaluate the function at x as a polynomial
-// using coefficients that sum up from start to end
-template <unsigned int n, class T>
-T evaluate_partial(const std::shared_ptr<POWERSERIES<n, T>> pwr, const std::vector<T>& x, const unsigned long start, const unsigned long end){
-  T ans;
-  for(unsigned long m=start; m<=end; m++){
-    auto P = iRRAM::partitions(m, n);
-    for(auto p : P){
-      T prod=1;
-      for(int i=0; i<n; i++){
-        prod *= power(x[i], p[i]);
+      int known_coeffs() const{
+	return 0;
       }
-      prod *= pwr->get(p);
-      ans += prod;
+    };
+
+    template <unsigned int n, class T>
+    template <typename... ARGS>
+    POWERSERIES<n,T>::POWERSERIES(std::function<T(const unsigned long, ARGS...)> f){
+      using seq = std::function<coeff_type(const unsigned long)>;
+      using sq_ptr = std::shared_ptr<seq>;
+      auto sq_fun = [f] (const unsigned long i) {
+	std::function<T(ARGS...)> pwr_fun = [f,i] (ARGS... rest) {return f(i, rest...);};
+	return POWERSERIES<n-1,T>(pwr_fun);
+      };
+      this->f = sq_ptr(new seq(sq_fun));
     }
-  }
-  return ans;
-}
-
-// add coefficients
-template <unsigned int n, class T>
-POWERSERIES<n,T> operator+(const POWERSERIES<n,T>& lhs, const POWERSERIES<n,T>& rhs){
-  return POWERSERIES<n,T>([lhs, rhs] (const std::vector<unsigned long>& v) {return lhs.get(v)+rhs.get(v);});
-}
-
-// subtract coefficients
-template <unsigned int n, class T>
-POWERSERIES<n,T> operator-(const POWERSERIES<n,T>& lhs, const POWERSERIES<n,T>& rhs){
-  return POWERSERIES<n,T>([lhs, rhs] (const std::vector<unsigned long>& v) {return lhs.get(v)-rhs.get(v);});
-}
 
 // fix the first parameter and return a POWERSERIES with decreased dimension
-template <unsigned int n, class T>
-typename PWRSERIES_TYPE<n-1,T>::type POWERSERIES<n,T>::operator[](const unsigned long i) const{
-  auto f=this->f;
-  return POWERSERIES<n-1,T>([f,i] (const std::vector<unsigned long>& v) {
-      std::vector<unsigned long> ind{i};
-      ind.insert(ind.end(), v.begin(), v.end());
-      return (*f)(ind);
-    });
-}
+    template <unsigned int n, class T>
+    typename COEFF_TYPE<n,T>::type POWERSERIES<n,T>::operator[](const unsigned long i) const{
+      return (*f)(i);
+    }
+    // add coefficients
+    template <unsigned int n, class T>
+    POWERSERIES<n,T> operator+(const POWERSERIES<n,T>& lhs, const POWERSERIES<n,T>& rhs){
+      return POWERSERIES<n,T>([lhs, rhs] (const unsigned int i) {return lhs[i]+rhs[i];});
+    }
 
-// for n=1 just return the scalar given by the coefficient
-// todo: since partial template specialization is not allowed
-// this only works for REAL at the moment.
-template <>
-typename PWRSERIES_TYPE<0,REAL>::type POWERSERIES<1,REAL>::operator[](const unsigned long i) const
-  {
-    return get({i});
-  }
+    // subtract coefficients
+    template <unsigned int n, class T>
+    POWERSERIES<n,T> operator-(const POWERSERIES<n,T>& lhs, const POWERSERIES<n,T>& rhs){
+      return POWERSERIES<n,T>([lhs, rhs] (const unsigned int i) {return lhs[i]-rhs[i];});
+    }
+
+
 // multiply coefficients
-template <unsigned int n, class T>
-POWERSERIES<n,T> operator*(const POWERSERIES<n,T>& lhs, const POWERSERIES<n,T>& rhs){
-  return POWERSERIES<n,T>([lhs, rhs] (const std::vector<unsigned long>& v) {
-      T c;
-      for(int l=0; l<=v[0];l++){
-	auto coeff = lhs[l]*rhs[v[0]-l];
-	std::vector<unsigned long> w;
-	w.insert(w.begin(), v.begin()+1, v.end());
-	c += coeff.get(w);
-      }
-      return c;
-    });
-}
-template <class T>
-POWERSERIES<1,T> operator*(const POWERSERIES<1,T>& lhs, const POWERSERIES<1,T>& rhs){
-  return POWERSERIES<1,T>([lhs, rhs] (const std::vector<unsigned long>& v) {
-      T c;
-      for(int l=0; l<=v[0];l++){
-	c += lhs[l]*rhs[v[0]-l];
-      }
-      return c;
-    });
-}
-} // PWRSERIES_IMPL namespace
-template <unsigned int n, class T>
-  using POWERSERIES = typename PWRSERIES_IMPL::PWRSERIES_TYPE<n,T>::type;
+    template <unsigned int n, class T>
+    POWERSERIES<n,T> operator*(const POWERSERIES<n,T>& lhs, const POWERSERIES<n,T>& rhs){
+      return POWERSERIES<n,T>([lhs, rhs] (const unsigned long i) {
+	  typename COEFF_TYPE<n,T>::type c;
+	  for(int l=0; l<=i;l++){
+	    auto coeff = lhs[l]*rhs[i-l];
+	    c = c+coeff;
+	  }
+	  return c;
+	});
+    }
+  } // PWRSERIES_IMPL namespace
+  template <unsigned int n, class T>
+  using POWERSERIES = PWRSERIES_IMPL::POWERSERIES<n,T>;
 } // iRRAM namespace
 
 #endif
--- test/powerseries.cc
+++ test/powerseries.cc
@@ -4,12 +4,21 @@
 using namespace std;
 using namespace iRRAM;
 void compute(){
-  using seq = std::function<REAL(const vector<unsigned long>&)>;
+  using seq = std::function<POWERSERIES<1,REAL>(const unsigned long)>;
   using seq_ptr = shared_ptr<seq>;
-  seq series = [] (const vector<unsigned long>& v) {int n=v[0]; int m=v[1]; return REAL(n+m);};
+  seq series = [] (const unsigned long n) {
+    auto subseq = [n] (unsigned long m) {return REAL(int(n+m));};
+    return POWERSERIES<1,REAL>(subseq);
+	  
+  };
+
+  function<REAL(unsigned long, unsigned long, unsigned long)> series2 = [] (unsigned long n, unsigned long m, const unsigned long k) {return REAL(int(n+m-k));};
+
   shared_ptr<POWERSERIES<2,REAL>> test(new POWERSERIES<2,REAL>(seq_ptr(new seq(series))));
-  iRRAM::cout << evaluate_partial(test, {3,5}, 2, 5) << endl;
-  iRRAM::cout << (*test)[3][1] << endl;
-  iRRAM::cout << ((*test)*(*test))[2][0] << endl;
+  POWERSERIES<3,REAL> test2(series2);
+  //iRRAM::cout << evaluate_partial(test, {3,5}, 2, 5) << endl;
+  iRRAM::cout << (*test)[300][245] << endl;
+  iRRAM::cout << test2[300][245][1000] << endl;
+  iRRAM::cout << ((*test)*(*test))[1][1] << endl;
 }
 
